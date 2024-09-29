bdf8327cd384a69a6cc43c3bba452c3101179a2a
nurfaidzy.riyamizard <nurfaidzy.riyamizard@akulaku.com>
2024-09-29 18:03:03 +0700

    feat: add font page n set theme

---
+++ lib/shared/theme.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+// The write is will be include about the opacity of colour
+Color kPrimaryColor = Color(0xFF5C40CC);
+Color kBlackColor = Color(0xFF1F1449);
+Color kWhiteColor = Color(0xFFFFFFFF);
+Color kGreyColor = Color(0xFF9698A9);
+Color kGreenColor = Color(0xFF0EC3AE);
+Color kRedColor = Color(0xFFEB70A5);
+Color kBackgroundColor = Color(0xFFFAFAFA);
+Color kInactiveColour = Color(0xFFDBD7EC);
+
+TextStyle blackTextStyle = TextStyle(
+  color: kBlackColor,
+);
+
+TextStyle whiteTextStyle = TextStyle(
+  color: kWhiteColor,
+);
+
+TextStyle greyTextStyle = TextStyle(
+  color: kGreyColor,
+);
+
+TextStyle greenTextStyle = TextStyle(
+  color: kGreenColor,
+);
+
+TextStyle redTextStyle = TextStyle(
+  color: kRedColor,
+);
+
+TextStyle purpleTextStyle = TextStyle(
+  color: kPrimaryColor,
+);
+
+FontWeight light = FontWeight.w300;
+FontWeight regular = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semiBold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
+FontWeight extraBold = FontWeight.w800;
+FontWeight black = FontWeight.w900;
