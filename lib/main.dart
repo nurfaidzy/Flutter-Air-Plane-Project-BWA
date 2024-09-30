94aaef26aefe64c083a2dc2cc244439f774330f9
nurfaidzy.riyamizard <nurfaidzy.riyamizard@akulaku.com>
2024-09-30 15:51:37 +0700

    feat: add route and update the main dart file

--- lib/main.dart
+++ lib/main.dart
@@ -11,10 +11,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      // home: SplashPage(),
-      home: getStartedPage(),
+      routes: {
+        "/": (context) => const SplashPage(),
+        "/get-started": (context) => const getStartedPage(),
+      },
     );
   }
 }
--- lib/ui/pages/splash_page.dart
+++ lib/ui/pages/splash_page.dart
@@ -1,8 +1,23 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import "../../shared//theme.dart";
 
-class SplashPage extends StatelessWidget {
+class SplashPage extends StatefulWidget {
   const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    Timer(const Duration(seconds: 3), () {
+      Navigator.pushNamed(context, "/get-started");
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
