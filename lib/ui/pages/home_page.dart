77a55037a194b7ddf68394120294cf67aab163b0
nurfaidzy.riyamizard <nurfaidzy.riyamizard@akulaku.com>
2024-10-13 17:10:39 +0700

    feat:  main ui of popular content

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -52,10 +52,105 @@ class HomePage extends StatelessWidget {
       );
     }
 
+    Widget popularDestination() {
+      return Container(
+        margin: const EdgeInsets.only(top: 30),
+        child: Row(
+          children: [
+            Container(
+              width: 200,
+              height: 323,
+              margin: EdgeInsets.only(left: defaultMargin),
+              padding: const EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                color: kWhiteColor,
+                borderRadius: BorderRadius.circular(defaultRadius),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                      width: 180,
+                      height: 220,
+                      margin: const EdgeInsets.only(bottom: 20),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(defaultRadius),
+                        image: const DecorationImage(
+                          image: AssetImage("assets/image_destination1.png"),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      child: Align(
+                        alignment: Alignment.topRight,
+                        child: Container(
+                          width: 55,
+                          height: 30,
+                          decoration: BoxDecoration(
+                            color: kWhiteColor,
+                            borderRadius: BorderRadius.only(
+                              bottomLeft: Radius.circular(
+                                defaultRadius,
+                              ),
+                            ),
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Container(
+                                width: 20,
+                                height: 20,
+                                margin: const EdgeInsets.only(right: 2),
+                                decoration: const BoxDecoration(
+                                  image: DecorationImage(
+                                    image: AssetImage("assets/icon_star.png"),
+                                  ),
+                                ),
+                              ),
+                              Text(
+                                "4.8",
+                                style: blackTextStyle.copyWith(
+                                  fontWeight: medium,
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      )),
+                  Container(
+                    margin: const EdgeInsets.only(left: 10),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Lake Ciliwung",
+                          style: blackTextStyle.copyWith(
+                            fontSize: 16,
+                            fontWeight: semiBold,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 5,
+                        ),
+                        Text(
+                          "Tanggerang",
+                          style: greyTextStyle.copyWith(
+                            fontSize: 14,
+                          ),
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      );
+    }
+
     return ListView(
-      children: [
-        Header(),
-      ],
+      children: [Header(), popularDestination()],
     );
   }
 }
