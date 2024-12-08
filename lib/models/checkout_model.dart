c6e99469c91993ddace3c7688e873dc4f63a7c0a
nurfaidzy.riyamizard <nurfaidzy.riyamizard@akulaku.com>
2024-12-08 20:50:29 +0700

    feat: modal checkout data

---
+++ lib/models/checkout_model.dart
@@ -0,0 +1,38 @@
+import 'package:equatable/equatable.dart';
+
+class CheckoutModel extends Equatable {
+  final String id;
+  final String idDestination;
+  final int totalTraveler;
+  final String selectedSeats;
+  final bool insurance;
+  final bool refundable;
+  final int vat;
+  final int price;
+  final int grandTotal;
+
+  CheckoutModel({
+    required this.id,
+    required this.idDestination,
+    required this.totalTraveler,
+    required this.selectedSeats,
+    required this.insurance,
+    required this.refundable,
+    required this.vat,
+    required this.price,
+    required this.grandTotal,
+  });
+
+  @override
+  List<Object?> get props => [
+        id,
+        idDestination,
+        totalTraveler,
+        selectedSeats,
+        insurance,
+        refundable,
+        vat,
+        price,
+        grandTotal,
+      ];
+}
